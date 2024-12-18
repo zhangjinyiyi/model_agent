1638b5cfa63e038fcbc1abaca3980de09029877b
zhangjinyiyi <zhangjinyi.cn@hotmail.com>
2024-12-18 17:21:49 +0800

    add claude support

--- tests/llm/test_claude.py
+++ tests/llm/test_claude.py
@@ -1,34 +1,31 @@
 #!/usr/bin/env python3
 # -*-coding:utf-8 -*-
 """
-@File    :   test_gpt.py
-@Time    :   2024/11/22 22:11:11
+@File    :   test_claude.py
+@Time    :   2024/12/18 16:51:44
 @Author  :   Jinyi Zhang 
 @Version :   1.0
 @Contact :   zhangjinyi.cn@hotmail.com
 @License :   (C)Copyright 2023-2024, Jinyi Zhang
 @Desc    :   None
 """
-
-# Test connection to OpenAI
-
 import time
 
-from modelagent.llm import GPTQuery
+from modelagent.llm.claude_bianxie import ClaudeQuery
+
 
 if __name__ == "__main__":
-    
     print("json model is disabled")
-    gpt = GPTQuery(model="gpt-4o")
+    claude = ClaudeQuery(model="claude-3-5-sonnet-20241022")
     start_time = time.time()
-    print(gpt.get_completion("where is the capital of France?"))
+    print(claude.get_completion("where is the capital of France?"))
     end_time = time.time()
     print(f"Time taken: {end_time - start_time} seconds")
     
     print("json model is enabled")
-    gpt = GPTQuery(model="gpt-4o", json_mode=True)
+    claude = ClaudeQuery(model="claude-3-5-sonnet-20241022", json_mode=True)
     start_time = time.time()
-    print(gpt.get_completion("where is the capital of France?"))
+    print(claude.get_completion("where is the capital of France?"))
     end_time = time.time()
     print(f"Time taken: {end_time - start_time} seconds")
-    
+
